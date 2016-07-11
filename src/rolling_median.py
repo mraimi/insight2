ee0a58a556f26ab5682b03e511be2764b7c41dab
Michael Raimi <mar2260@columbia.edu>
2016-07-11 17:56:24 -0400

    Update rolling_median.py

--- src/rolling_median.py
+++ src/rolling_median.py
@@ -51,8 +51,6 @@ def addEdge(adjList, target, actor, totalNodes):
 # Updates degree keys and counts when some number of records
 # must be pruned from the window
 def removeUpdate(pruned,adjList,degKeys,degCounts,totalNodes):
-	
-	print "prune: " + str(pruned)
 	for record in pruned:
 		target = record[0]
 		actor = record[1]
@@ -60,27 +58,24 @@ def removeUpdate(pruned,adjList,degKeys,degCounts,totalNodes):
 		
 		people = (target, actor)
 		for i in xrange(0, len(people)):
-			# print adjList[adjList.keys()[0]]
-			# print adjList[adjList.keys()[1]]
 			person = people[i] 
 			opp = people[int(not i)]
-			currDeg = adjList[person][0]
-			if (currDeg == 1):
-				totalNodes -= 1
-			print 'updated total:' + str(totalNodes)
+			currDeg = adjList[person][0]				
+			
+			# delete entries from degree keys and counts
 			if (degCounts[currDeg] == 1):
 				degKeys.remove(currDeg)
 				degCounts.pop(currDeg)
 			else:
 				degCounts[currDeg] -= 1
+			
 			if (currDeg == 1):
+				totalNodes -= 1
 				adjList.pop(person)
 			else:
 				adjList[person][0] = currDeg - 1
 				adjList[person][1].remove(opp)
-			print adjList[target][1]
 			
-
 	return totalNodes
 
 
@@ -100,7 +95,6 @@ def pruneIdx(window, newDate):
 def updateForInsert(adjList, target, actor, degCounts, degKeys):
 	for person in (target,actor):
 		d = adjList[person][0]
-		print person + " deg: %d" % d
 		if (d-1 > 0 and degCounts[d-1] > 0):
 			if (degCounts[d-1] == 1):
 				try:
@@ -112,22 +106,23 @@ def updateForInsert(adjList, target, actor, degCounts, degKeys):
 				degCounts[d-1] -= 1
 		if (d > 0):
 			degCounts[d] += 1
-		# print 'degCounts: ' + str(degCounts.keys())
 		insertKey(d, degKeys, degCounts)
 
 
 
 def getMedian(totalNodes, degCounts, degKeys):
-	for key in degCounts:
-		print "%d:%d" % (key,degCounts[key])
-	print "keys: "  + str(degKeys)
+	# for key in degCounts:
+		# print "%d:%d" % (key,degCounts[key])
 	isEven = totalNodes%2 == 0
 	remaining = totalNodes/2
 	for i in xrange(0,len(degKeys)):
 		key = degKeys[i]
 		diff = remaining - degCounts[key]
 		if (diff <= 0):
-			return (key+degKeys[i+1])/2.0 if (isEven and diff == 0) else key
+			if (len(degKeys) == 1):
+				return key
+			else:
+				return (key+degKeys[i+1])/2.0 if (isEven and diff == 0) else key
 		else:
 			remaining -= degCounts[key]
 
@@ -153,37 +148,33 @@ def old(newDate, window):
 	return False
 
 
-data = open('../data-gen/requiresInsert.txt', 'r')
+# data = open('../data-gen/requiresPruning.txt', 'r')
+data = open('../data-gen/venmo-trans.txt', 'r')
 adjList = defaultdict(lambda: [0, set()])
 window = []
 degKeys = []
 degCounts = defaultdict(int)
 totalNodes = 0
-
 ct = 0
-# 2016-03-28T23:23:12Z
 for line in data:
-	ct += 1
+	# ct += 1
 
 	record = json.loads(line.strip())
+	if (len(record) != 3):
+		continue
 	target, actor, date = record['target'], record['actor'], datetime.strptime(record['created_time'], '%Y-%m-%dT%H:%M:%SZ')
 	
 	
 	# Record is new
 	if (new(date, window)):
 		idx = pruneIdx(window,date)
-		print "idx: %d" % idx
-		print "total nodes before: %d" % totalNodes
 		totalNodes = removeUpdate(window[:idx],adjList,degKeys,degCounts,totalNodes)
-		print "total nodes after %d" % totalNodes
-
 		window = window[idx:]
 		window.append((target,actor,date))
 		totalNodes = addEdge(adjList, target, actor, totalNodes)
 		updateForInsert(adjList, target, actor, degCounts, degKeys)
 	# Record is out of order
 	else:
-		print 'out of order'
 		if(old(date, window)):
 			continue
 		totalNodes = addEdge(adjList, target, actor, totalNodes)
@@ -191,16 +182,9 @@ for line in data:
 		insertToWindow((target,actor,date), window)
 
 
-	print "median: " + str(getMedian(totalNodes, degCounts, degKeys)) + "\n"
-	print "total nodes: " + str(totalNodes)
-	# print window
-	# print degKeys
+	print "median: " + str(getMedian(totalNodes, degCounts, degKeys))
 
-	if (ct == 5):
-		break
+	# if (ct == 5):
+	# 	break
 
 data.close()
-
-# date1 = datetime.strptime('2016-03-28T23:23:19Z','%Y-%m-%dT%H:%M:%SZ')
-# date2 = datetime.strptime('2016-03-28T23:23:12Z','%Y-%m-%dT%H:%M:%SZ')
-# print (date1-date2).total_seconds()
